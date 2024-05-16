19fc3fb58b96d2881cce067828011055696ac8e6
tiennm2000 <tien.nm.2409@aptechlearning.edu.vn>
2024-05-17 00:41:42 +0700

    add json server and using fetch api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 import "./index.css";
@@ -7,24 +7,36 @@ import axios from "axios";
 export default function App() {
   const [books, setBooks] = useState([]);
 
+  const fetchBook = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    setBooks(response.data);
+  };
+
+  useEffect(() => {
+    fetchBook();
+  }, []);
+
   const createBook = async (title) => {
     const response = await axios.post("http://localhost:3001/books", {
       title,
     });
-    console.log(response);
     const updatedBooks = [...books, response.data];
     setBooks(updatedBooks);
   };
 
-  const deleteBook = (id) => {
+  const deleteBook = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
     const updatedBooks = books.filter((book) => book.id !== id);
     setBooks(updatedBooks);
   };
 
-  const editBook = (id, newTitle) => {
+  const editBook = async (id, newTitle) => {
+    const response = await axios.put("http://localhost:3001/books/" + id, {
+      title: newTitle,
+    });
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
       return book;
     });
